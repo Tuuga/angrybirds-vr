cf6037980a31ba552053beb96ff5fe353a5fb964
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2016-06-20 12:26:26 +0300

    Sannamaria scene, Block destruction improved (Turns off model and collider) + minColReg

--- angrybirds-vr-project/Assets/Scripts/Block.cs
+++ angrybirds-vr-project/Assets/Scripts/Block.cs
@@ -6,6 +6,9 @@ public class Block : MonoBehaviour {
 	[Tooltip("Force needed to destroy the block")]
 	public float durability;
 
+	[Tooltip("Minimum collision to register")]
+	public float minColReg;
+
 	Rigidbody rb;
 	ParticleSystem ps;
 
@@ -15,15 +18,21 @@ public class Block : MonoBehaviour {
 	}
 
 	void OnCollisionEnter (Collision c) {
-
-		durability -= c.impulse.magnitude;
-		if (durability <= 0) {
-			BlockDestroy();
-		}
+		if (c.impulse.magnitude > minColReg) {
+			durability -= c.impulse.magnitude;
+			if (durability <= 0) {
+				BlockDestroy();
+			}
+		}		
 	}
 
 	void BlockDestroy () {
+		var bc = GetComponent<BoxCollider>();
+		var model = transform.Find("Model").gameObject;
+
 		ps.Play();
+		bc.enabled = false;
+		model.SetActive(false);
 		Destroy(gameObject, ps.startLifetime);
 	}
 }
