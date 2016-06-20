c05b38ffd0e6a77e4ea3c5656b4f4818505d0929
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2016-06-20 09:47:25 +0300

    Blocks (wood, stone)

---
+++ angrybirds-vr-project/Assets/Scripts/Block.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	[Tooltip("Force needed to destroy the block")]
+	public float strenght;
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void OnCollisionEnter (Collision c) {
+		print(name + " - " + c.gameObject.name + " " + "Impulse: " + c.impulse.magnitude);
+		if (c.impulse.magnitude > strenght) {
+			BlockDestroy();
+		}
+	}
+
+	void BlockDestroy () {
+		// Particles in future
+		Destroy(gameObject);
+	}
+}
